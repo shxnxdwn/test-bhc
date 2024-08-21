078f7066dcd80eb01a4e53e3fc354dae9fa774d7
Gleb Kornilov <esplete@gmail.com>
2024-08-21 23:11:27 +0300

    end task 3

--- practice/task_3/js/filter-posts.js
+++ practice/task_3/js/filter-posts.js
@@ -1,11 +1,14 @@
-import {debounce} from './debounce.js';
+import { debounce } from './debounce.js';
 
 const filterClicked = {
-    userId: false,
-    postId: false,
-    postTitle: false,
-    postBody: false
+    userId: true,
+    postId: true,
+    postTitle: true,
+    postBody: true
 };
+
+let filteredPosts = [];
+
 
 const filterPosts = (postsArray, renderCb) => {
     const filterContainer = document.querySelector('.filter-container');
@@ -16,70 +19,63 @@ const filterPosts = (postsArray, renderCb) => {
         const inputValue = searchInput.value.trim().toLowerCase();
 
         if (inputValue.length > 2) {
-            const filteredPosts = postsArray.filter((post) =>
+            filteredPosts = postsArray.filter((post) =>
                 post.title.toLowerCase().includes(inputValue) || post.body.toLowerCase().includes(inputValue)
             );
-            renderCb(filteredPosts);
         } else {
-            renderCb(postsArray);
+            filteredPosts = postsArray.slice();
         }
+
+        renderCb(filteredPosts);
     };
 
 
     const sortPosts = (data, filterType) => {
         switch (filterType) {
             case 'user-id':
-                if (filterClicked.userId) {
-                    filterClicked.userId = false;
-                    return data.slice().sort((postA, postB) => postA.userId - postB.userId);
-                } else {
-                    filterClicked.userId = true;
-                    return data.slice().sort((postA, postB) => postB.userId - postA.userId);
-                }
+                filterClicked.userId = !filterClicked.userId;
+                return data.slice().sort((postA, postB) =>
+                    filterClicked.userId ? postA.userId - postB.userId : postB.userId - postA.userId
+                );
 
             case 'post-id':
-                if (filterClicked.postId) {
-                    filterClicked.postId = false;
-                    return data.slice().sort((postA, postB) => postA.id - postB.id);
-                } else {
-                    filterClicked.postId = true;
-                    return data.slice().sort((postA, postB) => postB.id - postA.id);
-                }
+                filterClicked.postId = !filterClicked.postId;
+                return data.slice().sort((postA, postB) =>
+                    filterClicked.postId ? postA.id - postB.id : postB.id - postA.id
+                );
 
             case 'post-title':
-                if (filterClicked.postTitle) {
-                    filterClicked.postTitle = false;
-                    return data.slice().sort((postA, postB) => postA.title.length - postB.title.length);
-                } else {
-                    filterClicked.postTitle = true;
-                    return data.slice().sort((postA, postB) => postB.title.length - postA.title.length);
-                }
+                filterClicked.postTitle = !filterClicked.postTitle;
+                return data.slice().sort((postA, postB) =>
+                    filterClicked.postTitle ? postA.title.length - postB.title.length : postB.title.length - postA.title.length
+                );
 
             case 'post-body':
-                if (filterClicked.postBody) {
-                    filterClicked.postBody = false;
-                    return data.slice().sort((postA, postB) => postA.body.length - postB.body.length);
-                } else {
-                    filterClicked.postBody = true;
-                    return data.slice().sort((postA, postB) => postB.body.length - postA.body.length);
-                }
+                filterClicked.postBody = !filterClicked.postBody;
+                return data.slice().sort((postA, postB) =>
+                    filterClicked.postBody ? postA.body.length - postB.body.length : postB.body.length - postA.body.length
+                );
 
             default:
-                return 0;
+                return data;
         }
     };
 
 
     filterContainer.addEventListener('click', (event) => {
         if (event.target.closest('.filter-container')) {
-            const [ filterClass ] = event.target.classList;
+            const [filterClass] = event.target.classList;
             const filterType = filterClass.replace('table__', '');
-            renderCb(sortPosts(postsArray, filterType));
+
+            filteredPosts = sortPosts(filteredPosts, filterType);
+            renderCb(filteredPosts);
         }
     });
 
     searchInput.addEventListener('input', debounce(searchPosts, 500));
-};
 
+    filteredPosts = postsArray.slice();
+    renderCb(filteredPosts);
+};
 
 export { filterPosts };
